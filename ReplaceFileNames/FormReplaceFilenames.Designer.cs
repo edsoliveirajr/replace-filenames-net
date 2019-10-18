cb4df6e954b8d9e512762480f11e65dfa95e1498
Edson de Oliveira Júnior <edsonoliveirajr@outlook.com>
2019-10-18 00:24:15 -0300

    initial commit

---
+++ ReplaceFileNames/FormReplaceFilenames.Designer.cs
@@ -0,0 +1,131 @@
+﻿namespace ReplaceFilenames
+{
+    partial class FormReplaceFilenames
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.txtPath = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtOldText = new System.Windows.Forms.TextBox();
+            this.txtNewText = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(558, 42);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtPath
+            // 
+            this.txtPath.Location = new System.Drawing.Point(92, 43);
+            this.txtPath.Name = "txtPath";
+            this.txtPath.Size = new System.Drawing.Size(443, 22);
+            this.txtPath.TabIndex = 1;
+            this.txtPath.Text = "C:\\Users\\edson\\Downloads";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(11, 46);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(37, 17);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Path";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 86);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(67, 17);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Substituir";
+            // 
+            // txtOldText
+            // 
+            this.txtOldText.Location = new System.Drawing.Point(92, 83);
+            this.txtOldText.Name = "txtOldText";
+            this.txtOldText.Size = new System.Drawing.Size(173, 22);
+            this.txtOldText.TabIndex = 4;
+            // 
+            // txtNewText
+            // 
+            this.txtNewText.Location = new System.Drawing.Point(362, 81);
+            this.txtNewText.Name = "txtNewText";
+            this.txtNewText.Size = new System.Drawing.Size(173, 22);
+            this.txtNewText.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(326, 84);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(30, 17);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Por";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(689, 151);
+            this.Controls.Add(this.txtNewText);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtOldText);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtPath);
+            this.Controls.Add(this.button1);
+            this.Name = "FormReplaceFilenames";
+            this.Text = "Replace Filenames";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.TextBox txtPath;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtOldText;
+        private System.Windows.Forms.TextBox txtNewText;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ ReplaceFileNames/FormReplaceFilenames.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ReplaceFilenames
+{
+    public partial class FormReplaceFilenames: Form
+    {
+        public FormReplaceFilenames()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string path = txtPath.Text;
+            string oldString = txtOldText.Text;
+            string newString = txtNewText.Text;
+
+            SubstituirNomeArquivos(path, oldString, newString, true);
+        }
+
+        /// <summary>
+        /// Substitui os nomes do arquivos da pasta
+        /// </summary>
+        /// <param name="folder"></param>
+        /// <param name="oldString"></param>
+        /// <param name="newString"></param>
+        /// <param name="mainFolder"></param>
+        private void SubstituirNomeArquivos(string folder, string oldString, string newString, 
+            bool mainFolder)
+        {
+            DirectoryInfo dirInfo = new DirectoryInfo(folder);
+
+            var files = dirInfo.GetFiles();
+
+            foreach (FileInfo f in files)
+            {
+                if (f.Name.Contains(oldString))
+                {
+                    string fileName = f.DirectoryName + "\\" + f.Name.Replace(oldString, newString);
+
+                    if (!File.Exists(fileName))
+                    {
+                        File.Move(f.FullName, fileName);
+                    }
+                }
+            }
+
+            var folders = dirInfo.GetDirectories();
+
+            foreach (var subFolder in folders)
+            {
+                SubstituirNomeArquivos(subFolder.FullName, oldString, newString, false);
+            }
+
+            if (dirInfo.Name.Contains(oldString))
+            {
+                string newDirectory = dirInfo.Parent.FullName + "\\" + dirInfo.Name.Replace(oldString, newString);
+
+                Directory.Move(dirInfo.FullName, newDirectory);
+            }
+        }
+    }
+}
